d01136ede5e06bfcc584e307f4226f79024905bc
Anurag Patil <aspanurag@gmail.com>
2020-08-28 21:29:30 -0400

    ci(lint-staged): remove git add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const App = () => {
   return <div></div>;
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
