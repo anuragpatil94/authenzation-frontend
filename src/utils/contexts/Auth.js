2ccac3695eaf2ec2dfa426124ed20c10488dd4dc
Anurag Patil <aspanurag@gmail.com>
2020-10-02 14:59:22 -0400

    feat(auth): add isAuthenticated flag| use localStorage hook for accesstoken and refreshtoken and authType| update api response

--- src/utils/contexts/Auth.js
+++ src/utils/contexts/Auth.js
@@ -3,12 +3,17 @@ import { useHistory } from 'react-router';
 
 import auth from '../auth';
 import { CONTEXT_DISPLAY_NAMES } from '../../constants';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 const AuthContext = React.createContext();
 AuthContext.displayName = CONTEXT_DISPLAY_NAMES.authContext;
 
 function AuthProvider(props) {
   const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [, setAccessToken] = useLocalStorage('accessToken', '');
+  const [, setRefreshToken] = useLocalStorage('refreshToken', '');
+  const [, setAuthType] = useLocalStorage('authType', 'basic');
   const history = useHistory();
 
   // Auth Functions
@@ -18,13 +23,21 @@ function AuthProvider(props) {
         .login(form)
         .then(({ success, data }) => {
           if (success) {
-            const { UserId, FirstName, LastName } = data.user;
-            setUser({ UserId, FirstName, LastName });
+            const { authType } = data;
+            if (authType === 'basic') {
+              const { UserId, FirstName, LastName } = data.user;
+              setUser({ UserId, FirstName, LastName });
+            } else if (authType === 'JWT') {
+              setAuthType(authType);
+              setAccessToken(data.tokens.accessToken);
+              setRefreshToken(data.tokens.refreshToken);
+            }
+            setIsAuthenticated(true);
             history.push('/');
           }
         })
         .catch(err => console.log(err)),
-    [history],
+    [setAccessToken, setRefreshToken, history, setAuthType, setIsAuthenticated],
   );
   const register = useCallback(
     formData =>
@@ -39,16 +52,20 @@ function AuthProvider(props) {
   const logout = useCallback(() => {
     auth.logout();
     setUser(null);
-  }, [setUser]);
+    setAccessToken('');
+    setRefreshToken('');
+    setIsAuthenticated(false);
+  }, [setUser, setAccessToken, setRefreshToken, setIsAuthenticated]);
 
   const value = React.useMemo(
     () => ({
       user,
+      isAuthenticated,
       login,
       logout,
       register,
     }),
-    [user, login, logout, register],
+    [user, login, logout, register, isAuthenticated],
   );
 
   return <AuthContext.Provider value={value} {...props} />;
