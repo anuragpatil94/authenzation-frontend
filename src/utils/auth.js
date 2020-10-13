5b5bf58649fef74434763b5987784cc605fd8e52
Anurag Patil <aspanurag@gmail.com>
2020-10-13 10:54:58 -0400

    feat: added forgetpassword route

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+const API_URL = 'http://localhost:4000/api/v1/auth';
+
 const login = async ({ authType, Username, Password }) => {
   // Check in database
   const res = await axios.post(
-    `http://localhost:4000/api/v1/auth/signin`,
+    `${API_URL}/signin`,
     {
       Username,
       Password,
@@ -29,7 +31,7 @@ const login = async ({ authType, Username, Password }) => {
 const register = async ({ FirstName, LastName, Username, Password }) => {
   // create new user in database
   // I think react query can be used here
-  const res = await axios.post(`http://localhost:4000/api/v1/auth/signup`, {
+  const res = await axios.post(`${API_URL}/signup`, {
     FirstName,
     LastName,
     Username,
@@ -45,9 +47,36 @@ const register = async ({ FirstName, LastName, Username, Password }) => {
   );
 };
 
-const logout = () => {
-  console.log('You just Logged Out');
-  return true;
+const forgotPassword = async ({ Username }) => {
+  console.log(Username);
+
+  const res = await axios.post(`${API_URL}/forget-password`, { Username });
+  const { success } = true;
+  return new Promise(
+    (resolve, reject) => res.status === 200 && resolve({ success }),
+  );
 };
 
-export default { login, register, logout };
+// const logout = async data => {
+//   let res = {};
+//   if (data.authType === 'basic') {
+//     res = {
+//       status: 200,
+//       success: true,
+//     };
+//   } else {
+//     res = await axios.post(`${API_URL}/signout`, null, {
+//       headers: {
+//         authtype: data.authType,
+//         Authorization: `Bearer ${data.accessToken}`,
+//       },
+//     });
+//     console.log('You just Logged Out');
+//     var { success } = res.data;
+//   }
+//   return new Promise(
+//     (resolve, reject) => res.status === 200 && resolve({ success }),
+//   );
+// };
+
+export default { login, register, forgotPassword };
